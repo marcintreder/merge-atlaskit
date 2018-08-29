4b37b71d781c9866bfdd131d02e5df9d431da3a6
Marcin Treder <marcin@uxpin.com>
2018-08-29 10:07:28 -0700

    Added uxpin-temp package to the repo

---
+++ .uxpin-temp/components.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Avatar from '../src/Avatar/Avatar';
+import Button from '../src/Button/Button';
+import ButtonGroup from '../src/ButtonGroup/ButtonGroup';
+import Calendar from '../src/Calendar/Calendar';
+import DynamicTable from '../src/DynamicTable/DynamicTable';
+import Wrapper from '../src/Wrapper/UXPinWrapper.js';
+export {
+  Avatar,
+  Button,
+  ButtonGroup,
+  Calendar,
+  DynamicTable,
+  Wrapper,
+  React,
+  ReactDOM,
+};
